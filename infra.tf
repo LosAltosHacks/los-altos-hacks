c882b5674dd931fc954e1c56071b6c6deafc8533
Josh Hejna <josh.hejna@gmail.com>
2019-09-08 03:03:39 -0700

    Add basic terraform config with S3 backend

---
+++ infra.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "0.12.8"
+  backend "s3" {
+    bucket = "terraform-backend-lah"
+    key    = "tf_state"
+    region = "us-west-2"
+  }
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "amis" {
+  default = {
+    us-west-2 = "ami-082b5a644766e0e6f"
+  }
+}
+
+provider "aws" {
+  version = "2.27.0"
+  profile = "default"
+  region  = var.region
+}
+
+# resource "aws_instance" "example" {
+#   ami           = var.amis[var.region]
+#   instance_type = "t2.micro"
+# }
