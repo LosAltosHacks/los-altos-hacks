beaa701e02c6386c0fd46fb7b5848bbe6eeb4ebe
Josh Hejna <josh.hejna@gmail.com>
2019-09-19 20:32:22 -0700

    Report status of stylecheck to stderr

--- scripts/stylecheck.sh
+++ scripts/stylecheck.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -eu
+trap 'printf "\e[1m\e[31mStyle checks failed.\e[0m\n" >&2' ERR
+
 npx prettier --check '**/*.html' '**/*.css' '**/*.js'
+
 echo "Checking for trailing whitespace..."
-grep --files-with-matches --binary-files=without-match '[[:blank:]]$' --dereference-recursive frontpage/ && exit 1
+grep --files-with-matches --binary-files=without-match '[[:blank:]]$' --dereference-recursive frontpage/ && false
+
+echo "Running ESLint"
 npx eslint 'frontpage/**/*.js'
+
+printf "\e[1m\e[32mStyle checks passed.\e[0m\n" >&2
