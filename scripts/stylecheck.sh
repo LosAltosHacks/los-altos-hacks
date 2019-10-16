a9b702681bbfc9a4d232077ed7f1558aba12f03b
Josh Hejna <josh.hejna@gmail.com>
2019-10-15 18:35:16 -0700

    Add a check for all text files requiring \n at EOF
    This isn't really needed; prettier does it for you for the things
    it supports. I realized that after writing it. I'm saving it
    because I like the script.

--- scripts/stylecheck.sh
+++ scripts/stylecheck.sh
@@ -2,10 +2,23 @@
 set -eu
 trap 'printf "\e[1m\e[31mStyle checks failed.\e[0m\n" >&2' ERR
 
+grep_exclude_pattern_file="/tmp/lah_exclude_file"
+cat <<EOF > "$grep_exclude_pattern_file"
+*.min.js
+*.svg
+EOF
+
+grep --binary-files=without-match --files-with-matches '.' \
+--dereference-recursive --exclude-from "$grep_exclude_pattern_file" ./frontpage | xargs -L 1 -I {} \
+bash -c '[ ! $(tail -c 1 {}) ] ||  { echo "{}: Missing newline at EOF."; false; }'
+
+
 npx prettier --check '**/*.html' '**/*.css' '**/*.js'
 
+
 echo "Checking for trailing whitespace..."
-grep --files-with-matches --binary-files=without-match '[[:blank:]]$' --dereference-recursive frontpage/ && false
+grep --files-with-matches --binary-files=without-match '[[:blank:]]$' \
+--dereference-recursive frontpage/ && false
 
 echo "Running ESLint"
 npx eslint 'frontpage/**/*.js'
