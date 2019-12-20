4ac576156b518af0523a0307b5aefbeb25da4e66
Leo Lin <mml0816@gmail.com>
2019-12-19 19:36:44 -0800

    import DBTimeslots

--- registration/backend/app/src/models/Mentor.py
+++ registration/backend/app/src/models/Mentor.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from .Person import DBPerson
+from .Timeslots import DBTimeslots
 
 
 class DBMentor(DBPerson):
@@ -10,7 +11,7 @@ class DBMentor(DBPerson):
     id = Column(Integer, ForeignKey('person.id'), nullable=False, primary_key=True)
     over_18 = Column(Boolean, nullable=False)
     skillset = Column(String(1000))
-    # timeslots = relationship("DBTimeslots", backref="mentor", order_by="DBTimeslots.start_time", lazy='selectin')
+    timeslots = relationship("DBTimeslots", backref="mentor", order_by="DBTimeslots.start_time", lazy='selectin')
 
     __mapper_args__ = {
         "polymorphic_identity": "mentors"
