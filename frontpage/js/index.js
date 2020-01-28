5b83dcac986daae4b0931d340b380fbc8952616c
Leo Lin <mml0816@gmail.com>
2020-01-28 12:20:56 -0800

    fix juniper logo

--- frontpage/js/index.js
+++ frontpage/js/index.js
@@ -169,7 +169,7 @@ function registerEvents() {
         ).style.transition = 'none';
 
         if (e.deltaY == 0) return;
-        clockTime += e.deltaY < 0 ? -5 : 5;
+        clockTime += e.deltaY < 0 ? -15 : 15;
 
         if (clockTime <= events[0].time) {
             clockTime = events[0].time;
