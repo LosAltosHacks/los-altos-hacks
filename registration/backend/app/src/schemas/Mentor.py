fa94104772813b6db7f4c64b3b2f43b904f324b5
Akash Vig <49599791+aksh1@users.noreply.github.com>
2021-03-01 15:42:00 -0800

    Update Mentor.py

--- registration/backend/app/src/schemas/Mentor.py
+++ registration/backend/app/src/schemas/Mentor.py
@@ -4,10 +4,26 @@ from .Attendee import ShirtSize
 
 
 class Mentor(BaseModel):
-    first_name: str
-    last_name: str
-    email: str
-    tshirt_size: ShirtSize
-    over_18: bool
-    skillset: str = None
-
+    first_name : str
+    last_name : str
+    email : str
+    phone_number : str
+    occupation : str
+    college_student : str = None
+    country : str
+    address_line_one : str
+    address_line_two : str
+    city : str
+    state_or_province : str = None
+    postal_code : str
+    timezone : str
+    birthdate : str
+    gender : str
+    ethnicity : str
+    shirt_size : ShirtSize
+    twitter_handle : str = None
+    github_username : str = None
+    programming_experience : int = None
+    technology_proficiency : int = None
+    proficent_in_languages : str = None
+    specific_skills : str = None
