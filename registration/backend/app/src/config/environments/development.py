46d150355a34e002c0cd57be68e639730dc598ca
Riderfighter <33171826+Riderfighter@users.noreply.github.com>
2021-03-14 20:22:33 -0700

    Added hackathon date for age calculations

--- registration/backend/app/src/config/environments/development.py
+++ registration/backend/app/src/config/environments/development.py
@@ -5,3 +5,4 @@ SQLALCHEMY_DATABASE_URL = "postgresql://lah_reg_postgres:5432/testregistration"
 SES_AWS_REGION = None
 SES_SENDER = None
 JWT_SECRET_KEY = "DEV_SECRET_IS_NOT_SECRET"
+HACKATHON_DATE = "2021-03-20"
