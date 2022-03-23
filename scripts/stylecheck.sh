2a11567ffe2afdaadd9b8b204d648993bd0f0e11
Ritam Saha <ritamsaha2005@gmail.com>
2022-03-22 19:52:45 -0700

    added eslint test back

--- scripts/stylecheck.sh
+++ scripts/stylecheck.sh
@@ -9,4 +9,7 @@ npx prettier --check '**/*.html' '**/*.css' '**/*.js' '**/*.yml' '**/*.yaml'
 echo "Checking for trailing whitespace..."
 grep --files-with-matches --binary-files=without-match '[[:blank:]]$' --dereference-recursive frontpage/ && false
 
+echo "Running ESLint"
+npx eslint 'frontpage/**/*.js'
+
 printf "\e[1m\e[32mStyle checks passed.\e[0m\n" >&2
