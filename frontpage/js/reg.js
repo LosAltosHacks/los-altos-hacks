0833f4f04eee253f40783df3f04cb86e2a1307b5
Ritam Saha <ritamsaha2005@gmail.com>
2022-03-22 17:49:51 -0700

    Used prettier

--- frontpage/js/reg.js
+++ frontpage/js/reg.js
@@ -327,11 +327,11 @@ function checkFilled($page) {
         if ($(e).attr('id') == 'shipping') {
             filledAll =
                 filledAll &&
-                ($('#en-shipping-country').val() != '' &&
+                $('#en-shipping-country').val() != '' &&
                     $('#en-shipping-addy1').val() != '' &&
                     $('#en-shipping-city').val() != '' &&
                     $('#en-shipping-stateprovince').val() != '' &&
-                    $('#en-shipping-postal').val() != '');
+                    $('#en-shipping-postal').val() != '';
         }
 
         var $input = $(e).find('input');
@@ -575,8 +575,8 @@ function registerAttendee() {
             },
             data: JSON.stringify(data),
         };
-        
-        $.ajax(settings).done(function (response) {
+
+        $.ajax(settings).done(function(response) {
             console.log(response);
             resolve(response);
         });
